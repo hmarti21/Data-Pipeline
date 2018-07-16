78324ecd0d5dc1b534b15b7828aa3923f315d17f
Hunter Martin <hmarti21@berkeley.edu>
2018-07-16 15:21:04 -0700

    Cleaning up Pipeline

--- corFunc.py
+++ corFunc.py
@@ -7,6 +7,7 @@ import numpy as np
 from astropy.table import Table
 import treecorr
 import h5py
+import matplotlib.pyplot as plt
 from astropy.io import ascii
 
 def div0( a, b ):
@@ -45,68 +46,53 @@ def projShapeCal(table):
     table['e2']=e2
     return table
 
-def NG(table,rand_pcounts,sightBins,nbins,min_sep,max_sep,rpar_step,fname,tableN=None):
-    table=noPadding(table,'ax')
+def NNCorProcess(cat1,cat2,sightBins,nbins,min_sep,max_sep,rpar_step):
+    corList=list()
+    for i in np.arange(sightBins):
+        corList.append(treecorr.NGCorrelation(nbins=nbins,min_sep=min_sep,max_sep=max_sep,metric='Rperp',
+                                             min_rpar=i*rpar_step,max_rpar=(i+1)*rpar_step))
+        corList[i].process(cat1,cat2,metric='Rperp')
+        return corList
+
+def NGCorProcess(catG,catN,sightBins,nbins,min_sep,max_sep,rpar_step):
+    corList=list()
+    pairCounts=np.zeros((sightBins,nbins))
+    for i in np.arange(sightBins):
+        corList.append(treecorr.NGCorrelation(nbins=nbins,min_sep=min_sep,max_sep=max_sep,metric='Rperp',
+                                             min_rpar=i*rpar_step,max_rpar=(i+1)*rpar_step))
+        corList[i].process(catN,catG,metric='Rperp')
+        pairCounts[i]=corList[i].npairs
+    return corList,pairCounts
+    
+def NG(table,rand_pcounts,sightBins,nbins,min_sep,max_sep,rpar_step,fname,rscale,min_box,max_boxtableN=None):
     table=projShapeCal(table)
-    NGCor=list()
     if tableN==None:
         catG=treecorr.Catalog(x=table['x'],y=table['y'],z=table['z'],g1=table['e1'],g2=table['e2'])
-        for i in np.arange(sightBins):
-            NGCor.append(treecorr.NGCorrelation(nbins=nbins,min_sep=min_sep,max_sep=max_sep,metric="Rperp",
-                                                min_rpar=i*rpar_step,max_rpar=(i+1)*rpar_step))
-            NGCor[i].process(catG,catG,metric='Rperp')
+        NGCor,NGCorCounts=NGCorProcess(catG,catG,sightBins,nbins,min_sep,max_sep,rpar_step)
     else:
         catG=treecorr.Catalog(g1=table['e1'],g2=table['e2'])
         catN=treecorr.Catalog(x=tableN['x'],y=tableN['y'],z=tableN['z'])
-        for i in np.arange(sightBins):
-            NGCor.append(treecorr.NGCorrelation(nbins=nbins,min_sep=min_sep,max_sep=max_sep,metric="Rperp",min_rpar=i*rpar_step,max_rpar=(i+1)*rpar_step))
-            NGCor[i].process(catN,catG,metric='Rperp')
+        NGCor,NGCorCounts=NGCorProcess(catN,catG,sightBins,nbins,min_sep,max_sep,rpar_step)
     NG_ksi=np.zeros((sightBins,nbins))
     for i in np.arange(sightBins):
-        NG_ksi[i]=div0(NGCor[i].npairs,rand_pcounts[i])
+        NG_ksi[i]=div0(NGCorCounts[i],rand_pcounts[i])
     NG_w=np.sum(NG_ksi,axis=0)
     np.savetxt(fname,NG_w)
+    return NG_w
 
 def NN(table,rand_cat,sightBins,nbins,min_sep,max_sep,rpar_step,fname,table2=None):
     catN=treecorr.Catalog(x=table['x'],y=table['y'],z=table['z'])
     if table2!=None:
-        catN2=treecorr.Catalog(x=table2['x'],y=table2['y'],z=table2['z'])
-    NNCor=list()
-    NRCor=list()
-    RRCor=list()
-    RNCor=list()
-    NNPairs=np.zeros((sightBins,nbins))
-    NRPairs=np.zeros((sightBins,nbins))
-    RNPairs=np.zeros((sightBins,nbins))
-    RRPairs=np.zeros((sightBins,nbins))
-    for i in np.arange(sightBins):
-        NNCor.append(treecorr.NNCorrelation(nbins=nbins,min_sep=min_sep,max_sep=max_sep,metric="Rperp",
-                                               min_rpar=i*rpar_step,max_rpar=(i+1)*rpar_step))
-        NRCor.append(treecorr.NNCorrelation(nbins=nbins,min_sep=min_sep,max_sep=max_sep,metric="Rperp",
-                                               min_rpar=i*rpar_step,max_rpar=(i+1)*rpar_step))
-        RRCor.append(treecorr.NNCorrelation(nbins=nbins,min_sep=min_sep,max_sep=max_sep,metric="Rperp",
-                                               min_rpar=i*rpar_step,max_rpar=(i+1)*rpar_step))
-        if table2==None:
-            NNCor[i].process(catN,metric='Rperp')
-            NRCor[i].process(catN,rand_cat,metric='Rperp')
-            RRCor[i].process(rand_cat,metric='Rperp')
-            NNPairs[i]=NNCor[i].npairs
-            NRPairs[i]=NRCor[i].npairs
-            RRPairs[i]=RRCor[i].npairs
-            print(NNCor[i].npairs)
-            print(NRCor[i].npairs)
-            print(RRCor[i].npairs)
-        else:
-            RNCor.append(treecorr.NNCorrelation(nbins=nbins,min_sep=min_sep,max_sep=max_sep,metric='Rperp',
-                                              min_rpar=i*rpar_step,max_rpar=(i+1)*rpar_step))
-            NNCor[i].process(catN,catN2,metric='Rperp')
-            NRCor[i].process(catN,rand_cat,metric='Rperp')
-            RNCor[i].process(catN2,rand_cat,metric='Rperp')
-            RRCor[i].process(rand_cat,rand_cat,metric='Rperp')
-            NNPairs[i]=NNCor[i].npairs
-            NRPairs[i]=NRCor[i].npairs
-            RNPairs[i]=RNCor[i].npairs
-            RRPairs[i]=RRCor[i].npairs
+        catN2=treecorr.Catalog(x=table2['x'],y=table2['y'],z=table['z'])
+    if table2==None:
+        NNCor=NNCorProcess(catN,catN,sightBins,min_sep,max_sep,rpar_step)
+        NRCor=NNCorProcess(catN,rand_cat,sightBins,min_sep,max_sep,rpar_step)
+        RRCor=NNCorProcess(rand_cat,rand_cat,sightBins,min_sep,max_sep,rpar_step)
+    else:
+        NNCor=NNCorProcess(catN,catN2,sightBins,min_sep,max_sep,rpar_step)
+        NRCor=NNCorProcess(catN,rand_cat,sightBins,min_sep,max_sep,rpar_step)
+        RNCor=NNCorProcess(catN2,rand_cat,sightBins,min_sep,max_sep,rpar_step)
+        RRCor=NNCorProcess(rand_cat,rand_cat,sightBins,min_sep,max_sep,rpar_step)
     xi=np.zeros((nbins,sightBins))
     varxi=np.zeros((nbins,sightBins))
     for i in np.arange(sightBins):
@@ -117,22 +103,18 @@ def NN(table,rand_cat,sightBins,nbins,min_sep,max_sep,rpar_step,fname,table2=Non
     xi_tot=np.sum(xi,axis=1)
     varxi_tot=np.sum(varxi,axis=1)
     np.savetxt(fname, np.array([xi_tot,varxi_tot]))
-    print(NNPairs,NRPairs,RNPairs,RRPairs)
-    return NNPairs,NRPairs,RNPairs,RRPairs
+    return xi_tot,varxi_tot
 
 def RR(table,scale,min_box,max_box,sightBins,nbins,min_sep,max_sep,rpar_step,cat_out=False):
     rand=np.random.uniform(min_box,max_box,size=len(table['x'])*scale*3)
-    rand=np.random.permutation(rand).reshape(3,len(table['x'])*scale)
-    print(min_box,max_box,np.amin(rand),np.amax(rand))
+    rand.np.random.permutation(rand).reshape(3,len(table['x']*scale))
     catR=treecorr.Catalog(x=rand[0],y=rand[1],z=rand[2])
     if cat_out==True:
         return catR
     RRCor=list()
     rand_pcounts=np.zeros((sightBins,nbins))
-    for i in np.arange(sightBins):
-        RRCor.append(treecorr.NNCorrelation(nbins=nbins,min_sep=min_sep,max_sep=max_sep,metric="Rperp",
-                                            min_rpar=i*rpar_step,max_rpar=(i+1)*rpar_step))
-        RRCor[i].process(catR,catR,metric='Rperp')
+    RRCor=NNCorProcess(catR,catR,sightBins,nbins,min_sep,max_sep,rpar_step)
+    for i in np.arange(sightBins)
         rand_pcounts[i]=RRCor[i].npairs
     return rand_pcounts
 
@@ -149,32 +131,34 @@ def corFunc(fname1,col_name,cuts,sightBins,rscale,nbins,min_sep,max_sep,rpar_ste
     log=open(logfile,'w')
     log.write(log_file)
     log.write(fname1)
-    NNPairs=np.zeros((sightBins,nbins))
-    NRPairs=np.zeros((sightBins,nbins))
-    RRPairs=np.zeros((sightBins,nbins))
-    RNPairs=np.zeros((sightBins,nbins))
-
     if func=='NN' and fname2==None:
         catR=RR(data,rscale,min_box,max_box,sightBins,nbins,min_sep,max_sep,rpar_step,cat_out=True)
-        NNPairs,NRPairs,RNPairs,RRPairs=NN(data,catR,sightBins,nbins,min_sep,max_sep,rpar_step,savefile)
+        xi,varxi=NN(data,catR,sightBins,nbins,min_sep,max_sep,rpar_step,savefile)
         log.close()
-        return NNPairs,NRPairs,RNPairs,RRPairs
+        return xi,varxi
     if func=='NG' and fname2==None:
         data=noPadding(data,'ax')
         rand_pairs=RR(data,rscale,min_box,max_box,sightBins,nbins,min_sep,max_sep,rpar_step)
-        NG(data,rand_pairs,sightBins,nbins,min_sep,max_sep,rpar_step,savefile)
+        NG_w=NG(data,rand_pairs,sightBins,nbins,min_sep,max_sep,rpar_step,savefile)
         log.close()
+        return NG_w
     if fname2!=None:
         log.write('------2ND FILE------')
-        data2=fname2 #read_Data_hdf5(fname2,col_name2,cuts2,key2)
+        data2=fname2#read_Data_hdf5(fname2,col_name2,cuts2,key2)
         if func=='NN':
-            print(data2)
             catR=RR(data,rscale,min_box,max_box,sightBins,nbins,min_sep,max_sep,rpar_step,cat_out=True)
-            NN(data,catR,sightBins,nbins,min_sep,max_sep,rpar_step,savefile,table2=data2)
+            xi,varxi=NN(data,rscale,min_box,max_box,sightBins,nbins,min_sep,max_sep,rpar_step,table2=data2)
             log.close()
+            return xi, varxi
         if func=='NG':
             data=noPadding(data,'ax')
             rand_pairs=RR(data,rscale,min_box,max_box,sightBins,nbins,min_sep,max_sep,rpar_step)
-            NG(data,rand_pairs,sightBins,nbins,min_sep,max_sep,rpar_step,savefile,tableN=data2)
+            NG_w=NG(data,rand_pairs,sightBins,nbins,min_sep,max_sep,rpar_step,savefile,tableN=data2)
             log.close()
+            return NG_w
+
+
+# In[ ]:
+
+
 
